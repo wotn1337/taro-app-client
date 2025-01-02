b7a7d3d5095376637730e9545bed51fe6aa89a57
Артем Белов <theflash.0320@gmail.com>
2025-01-02 16:54:00 +0500

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
     },
-  },
+    preview: {
+      host: env.VITE_PREVIEW_HOST,
+      port: Number(env.VITE_PREVIEW_PORT),
+    },
+  };
 });

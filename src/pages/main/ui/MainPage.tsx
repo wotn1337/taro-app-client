55d5518d4f6f9549276b7cc49d1f8318beceb7db
Артем Белов <theflash.0320@gmail.com>
2025-01-03 18:08:06 +0500

    delete unused user var

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,12 +1,9 @@
-import { selectUser } from "@/entities/user";
 import { CardsMenu } from "@/widgets/cards-menu";
 import { FC } from "react";
-import { useSelector } from "react-redux";
 
 type Props = {};
 
 export const MainPage: FC<Props> = ({}) => {
-  const user = useSelector(selectUser);
   return (
     <>
       <CardsMenu />
